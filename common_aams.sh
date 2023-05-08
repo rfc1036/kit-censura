2fbc6352e4c77aadb7cf45e43f9e77621606d907
root <root@fw.ictvalleumbra.it>
2023-05-08 11:06:12 +0200

    Better error handling

--- common_aams.sh
+++ common_aams.sh
@@ -10,9 +10,36 @@ fi
 
 download_aams() {
   local output=$1
+  local disable_alert=$2
 
+  local FILE_aams0='tmp/landing_page.html'
   local FILE_aams1='tmp/elenco_siti_inibiti.txt'
   local FILE_aams2='tmp/elenco_siti_inibiti.sha-256'
+
+if [ "x$disable_alert" != "x" ]  ; then
+	NOC_EMAIL=""
+fi
+
+  curl --fail --location --remote-time $CURL_OPTS_aams \
+    --output $FILE_aams0.tmp $URL_aams
+  CURL_RETURN=$?
+  if [ $CURL_RETURN != 0 ] ; then
+        SUBJECT="Error while fetching Censura lists"
+        TXT="Curl on $(hostname -f) have returned $CURL_RETURN:\n\n${curl_errors[$CURL_RETURN]}\n\n when trying to get $URL_aams."
+        if [ $ALERT_ENABLE == true ] && [ "x$NOC_EMAIL" != 'x' ] ; then
+                echo -e "Subject: $SUBJECT\nFrom:$FROM_EMAIL\n$TXT" | sendmail $NOC_EMAIL
+        fi
+        echo "Warning: $TXT" >&2
+  else
+        test $LOGGING_ENABLE == true && echo "$(date '+%d/%m/%y %H:%m:%S') - Successfully downloaded file $URL_aams" >> $LOGFILE
+  fi
+  mv $FILE_aams0.tmp $FILE_aams0
+
+  ELENCO=$(cat $FILE_aams0 | grep txt | grep -i Elenco | cut -d\" -f2) 
+  CONTROLLO=$(cat $FILE_aams0 | grep txt | grep -i Controllo | cut -d\" -f2)
+  BASE=$(echo $URL_aams | cut -d\/ -f-3)
+  URL_aams1="$BASE$ELENCO"
+  URL_aams2="$BASE$CONTROLLO"
 
   curl --fail --location --remote-time $CURL_OPTS_aams \
     --output $FILE_aams1.tmp $URL_aams1
