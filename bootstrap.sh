073accb865ef17d0ea072cfb7ea4de5ab00de8bb
Antonio Bartolini <antonio.bartolini@connesi.it>
2024-02-19 11:59:07 +0100

    new bootsrap file

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x 
+
+echo Downloading external tools
+
+wget "https://github.com/mphilosopher/censura/raw/master/src/download_agcom.py" -O download_agcom.py
+chmod +x download_agcom.py
+
+wget "https://github.com/mphilosopher/censura/raw/master/src/download_consob.py" -O download_consob.py
+chmod +x download_consob.py
+
+wget "https://github.com/grelleum/supernets/raw/master/supernets.py" -O supernets.py
+chmod +x supernets.py
+
+
+echo Creating new empty files:
+
+touch lista.manuale
+touch lista.manuale-ip
+
+
+echo Creating dirs
+mkdir tmp lists
+
+
