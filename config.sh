5f2a9398b0198e8ee62a96d5ab5c6903d70d38e9
Antonio Bartolini <antonio.bartolini@connesi.it>
2024-05-22 15:20:53 +0200

    Update readme

--- config.sh
+++ config.sh
@@ -111,7 +111,7 @@ CNCPO_REPLY_PASSWORD=$CNCPO_IMAP_PSWD
 # Underscore variables (_LISTAID_ , _LISTADATE_ and _DATE_) will be replaced automatically
 CNCPO_REPLY_TEMPLATE="Buongiorno,
 con la presente si segnala che in data _DATE_ e' avvenuta ricezione e applicazione della lista dei siti da inibire per il CNCPO avente progressivo _LISTAID_ e identificativo _LISTADATE_.
-Il messaggio e stato generato automaticamente, pertanto vi preghiamo di segnalare qualsiasi eventuale problema o incorrettezza del presente riscontro.
+Il messaggio e' stato generato automaticamente, pertanto vi preghiamo di segnalare qualsiasi eventuale problema o incorrettezza del presente riscontro.
 
 Cordiali Saluti.
 Acme S.p.A.
