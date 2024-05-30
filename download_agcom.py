909ca7c22164b1612c4cc070ffa34efba286904b
Antonio Bartolini <antonio.bartolini@connesi.it>
2024-05-30 10:22:01 +0200

    New version by Federico Santulli - NHM - S.R.L.
    * Recupera anche le Determine DDA (la lista include sia DELIBERE che DETERMINE);
    * Implementato Referer e User-Agent su richieste (evita detect come scraper);
    * Implementata Attesa Randomica prima di effettuare richieste (evita detect come scraper);
    * Implementato Allow Redirect per download richieste (non è più abilitato di default su requests

--- download_agcom.py
+++ download_agcom.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python3
 
-import requests, optparse, ssl
-from bs4 import BeautifulSoup
+import requests, optparse, sys
 from urllib3.exceptions import InsecureRequestWarning
-requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+from bs4 import BeautifulSoup
+from random import randint
+from time import sleep
+import ua_generator
 
-# Set default SSL context to use TLS 1.2
-ssl_context = ssl.create_default_context()
-ssl_context.set_ciphers('DEFAULT:@SECLEVEL=1')
-ssl_context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
-ssl_context.minimum_version = ssl.TLSVersion.TLSv1_2
+# Funzione di Attesa Randomica per evitare di essere identificati come bot di Scraping
+def waitRand():
+    waitms = randint(100,2000)/1000
+    sleep(waitms)
 
 def main():
     global options
-# Definisco le tre variabili sotto e le inizializzo con valori a caso
-# Questo perchè se in tutta la pagina non trovo un provvedimento
-# allora determina,lastDetermina e allegatoB restano non inizializzate e mandano
-# in errore lo script
 
-    global delibera
-    global lastDelibera
+    # Variabili
+    global provvedimento
+    global lastProvvedimento
     global allegatoB
 
+    # Aggiunte Variabili per Evitare di essere identificati come bot di Scraping
+    global referer
+    global user_agent
 
-    delibera = ""
-    lastDelibera = "https://www.agcom.it/provvedimenti-a-tutela-del-diritto-d-autore"
+    provvedimento = ""
+    lastProvvedimento = "https://www.agcom.it/provvedimenti-a-tutela-del-diritto-d-autore"
     allegatoB = "https://www.example.com"
+    referer = lastProvvedimento
 
+    # Imposto User Agent Randomico
+    user_agent = ua_generator.generate(device='desktop', platform=('windows', 'macos', 'linux'), browser=('chrome', 'edge', 'firefox', 'safari')).text
 
     # Elaborazione argomenti della linea di comando
     usage = "usage: %prog [options] arg"
@@ -39,22 +43,35 @@ def main():
     if (options.out_file is None):
         parser.error("Numero di argomenti non corretto")
 
+    # Disabilita i warning relativi alla mancata verifica dei certificati
+    requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
     curpage=1
-    lastDelibera = None
-    while((curpage<10) and (lastDelibera is None)):        
-        url = "https://www.agcom.it/provvedimenti-a-tutela-del-diritto-d-autore?p_p_id=listapersconform_WAR_agcomlistsportlet&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&p_p_col_id=column-1&p_p_col_count=1&_listapersconform_WAR_agcomlistsportlet_numpagris=50&_listapersconform_WAR_agcomlistsportlet_curpagris={}".format(curpage)
-        page = requests.get(url, verify=False)
+    lastProvvedimento = None
+    while((curpage<10) and (lastProvvedimento is None)):
+        url = "https://www.agcom.it/provvedimenti-a-tutela-del-diritto-d-autore?p_p_id=listapersconform_WAR_agcomlistsportlet&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&p_p_col_id=column-1&p_p_col_count=1&_listapersconform_WAR_agcomlistsportlet_numpagris=10&_listapersconform_WAR_agcomlistsportlet_curpagris={}".format(curpage)
+        if(curpage > 1): waitRand()
+        s = requests.Session()
+        s.headers.update({'referer': referer, 'user-agent': user_agent })
+        page = s.get(url, verify=False)
+        referer = url
         soup = BeautifulSoup(page.content, "html.parser")
         for div in soup.findAll('div', attrs={'class':'risultato'}):
-            if lastDelibera: break
+            if lastProvvedimento: break
             for p in div.findAll('p'):
-                if ((p.text.lower().find("provvedimento")==-1) and (p.text.lower().find("ordine")==-1)):continue
-                    #determina = div.find(lambda tag:(tag.name=="a" and (tag.text.lower().find("determina")!=-1) or (tag.text.lower().find("delibera")!=-1)))
-                delibera = div.find(lambda tag:(tag.name=="a" and tag.text.lower().find("delibera")!=-1))
-                if delibera:
-                    lastDelibera = "https://www.agcom.it"+delibera["href"]
+                if ((p.text.lower().find("provvedimento")==-1) and (p.text.lower().find("ordine")==-1)): continue
+                provvedimento = None
+                for a in div.findAll('a'):
+                    if (a.text.lower().find("delibera")!=-1 or a.text.lower().find("determina")!=-1):
+                        provvedimento = a
+                        break
+                if provvedimento:
+                    lastProvvedimento = "https://www.agcom.it"+provvedimento["href"]
                     #### Check Allegato ######
-                    page = requests.get(lastDelibera, verify=False)
+                    waitRand()
+                    s = requests.Session()
+                    s.headers.update({'referer': referer, 'user-agent': user_agent })
+                    page = s.get(lastProvvedimento, allow_redirects=True, verify=False)
                     soup = BeautifulSoup(page.content, "html.parser")
                     # Controllo se ho trovato un Allegato B vedendo se la variabile inizializzata è stata modificata
                     # Solo se allegatoB è stato trovato, allora lo elaboro
@@ -63,14 +80,17 @@ def main():
                         allegatoB = allegato["href"]
                         break
                     if not "www.example.com" in allegatoB:
-                        response = requests.get(allegatoB, verify=False)
+                        waitRand()
+                        s = requests.Session()
+                        s.headers.update({'referer': referer, 'user-agent': user_agent })
+                        response = s.get(allegatoB, allow_redirects=True, verify=False)
                         with open(options.out_file,'wb') as f:
                             f.write(response.content)
                     else:
-                        lastDelibera = None
+                        lastProvvedimento = None
                     break
         curpage = curpage + 1
-    if lastDelibera is None:
+    if lastProvvedimento is None:
         return(False)
 
 if __name__ == '__main__':
