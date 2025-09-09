9edfd4b37a4a7120366839dce8797651fa24a160
Antonio Bartolini <robynhub@gmail.com>
2025-09-09 19:46:33 +0200

    Update download_consob.py
    Comment fix

--- download_consob.py
+++ download_consob.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# -Written without shame by ChatGPT-
+# ---------------------------
+# Written by IA
+# ---------------------------
 
 import re
 import sys
