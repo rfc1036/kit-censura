afda2b2c0f7336918146f2fd47c32c81e596dbe4
Antonio Bartolini <antonio.bartolini@connesi.it>
2025-09-10 09:20:19 +0200

    Fixed filename typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,13 @@
 #!/bin/bash -x 
 
-echo Downloading external tools
-
-# use local copy
-#wget "https://github.com/mphilosopher/censura/raw/master/src/download_agcom.py" -O download_agcom.py
-#chmod +x download_agcom.py
-#
-#wget "https://github.com/mphilosopher/censura/raw/master/src/download_consob.py" -O download_consob.py
-#chmod +x download_consob.py
+chmod +x download_*.py
 
 echo Creating new empty files:
 
-touch lista.manuale
-touch lista.manuale-ip
+for lista in manuale consob agcom aams tabacchi pscaiip; do
+	touch lista.$lista
+	touch lista.$lista-ip
+done
 
 
 echo Creating dirs
